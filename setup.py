2f474e25eb44d5e4168c5096a3e072b904f9db27
shreedhar13 <shreedharjagatap2002@gmail.com>
2024-07-24 18:18:17 +0530

    updated ipynb file

--- setup.py
+++ setup.py
@@ -1,1 +1,11 @@
 #To install local packages in my local environment (my_proj_env)
+from setuptools import find_packages,setup
+
+setup(
+    name="mcq_generator",
+    version="0.0.1",
+    author="shreedhar sanjay jagatap",
+    author_email="shreedharjagatap2002@gmail.com",
+    install_requires=['openai','langchain','streamlit','python-dotenv','PyPDF2'],
+    packages=find_packages()
+)
